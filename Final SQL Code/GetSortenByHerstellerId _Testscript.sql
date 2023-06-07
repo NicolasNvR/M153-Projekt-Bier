0728fe58f64b21001f3d3a300bf25d677dbdf2da
Jeremy <71987751+JeremyKrummenacher@users.noreply.github.com>
2023-06-07 14:29:10 +0200

    added comments on what it tests

--- Final SQL Code/GetSortenByHerstellerId _Testscript.sql
+++ Final SQL Code/GetSortenByHerstellerId _Testscript.sql
@@ -1,18 +1,24 @@
 use Bierverwaltung
 
+--Die Funktion wird mit dem Parameter @HerstellerId aufgerufen, der den Wert 1 enthält.
+--Die SELECT-Anweisung gibt die Sortennamen und den Herstellernamen für den Hersteller mit der ID 1 zurück
 DECLARE @HerstellerId INT = 1; -- Hersteller mit Id 1 
 
 SELECT SortenName, HerstellerName
 FROM dbo.fn_GetSortenByHerstellerId(@HerstellerId);
 go
 
-DECLARE @HerstellerId INT = 0; -- Nicht vorhandene HersellerId
+--Die Funktion wird mit dem Parameter @HerstellerId aufgerufen, der den Wert 0 enthält.
+--Die SELECT-Anweisung gibt NULL-Werte für Sortenname und Herstellername zurück, da keine entsprechende Hersteller-ID gefunden wurde.
+DECLARE @HerstellerId INT = 0; -- Nicht vorhandene HerstellerId
 
 SELECT SortenName, HerstellerName
 FROM dbo.fn_GetSortenByHerstellerId(@HerstellerId);
 go
 
-DECLARE @HerstellerId INT = 2; -- Hersteller mit Id 2
+--Die Funktion wird mit dem Parameter @HerstellerId aufgerufen, der den Wert 2 enthält.
+--Die SELECT-Anweisung gibt die Sortennamen und Herstellernamen für den Hersteller mit der ID 2 zurück.
+DECLARE @HerstellerId INT = 2;
 
 SELECT SortenName, HerstellerName
 FROM dbo.fn_GetSortenByHerstellerId(@HerstellerId);
